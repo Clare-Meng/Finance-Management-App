04955f9e8f06923488f27407eb0102dd54ef8338
Clare <claremeng31203@yahoo.ca>
2021-03-03 22:44:40 -0700

    Added navigation bar

---
+++ client/src/NavigationBar.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavbarText
+  } from 'reactstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './NavigationBar.css';
+
+class NavigationBar extends React.Component {
+
+    state = {
+        isOpen: false
+    }
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        return (
+            <>
+               <div>
+                    <Navbar color="light" light expand="md">
+                        <NavbarBrand href="/">Home</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="mr-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/monthly_budget">Monthly Budget</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/">Planning</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/">Total Budget</NavLink>
+                            </NavItem>
+                            <UncontrolledDropdown nav inNavbar>
+                                <DropdownToggle nav caret>
+                                    Options
+                                </DropdownToggle>
+                                <DropdownMenu right>
+                                    <DropdownItem>
+                                        Spending History
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        Budget Summary
+                                    </DropdownItem>
+                                    <DropdownItem divider />
+                                    <DropdownItem>
+                                        Sign Out
+                                    </DropdownItem>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Nav>
+                        </Collapse>
+                    </Navbar>
+                </div>
+            </>
+        );
+    }
+}
+
+export default NavigationBar;
